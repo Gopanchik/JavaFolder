e713321d871ac907a0624d8826ae54c843b687db
sanyo <egp.2277@gmail.com>
2023-09-27 14:27:21 +0300

    first commit

---
+++ src/lesson1/Name.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+ public class Name {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i++){
+            System.out.println("Egor");
+        }
+    }
+}
