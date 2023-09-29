fd3392698363cf4fa2a66f9d0eb9b95f27a9e7cf
sanyo <egp.2277@gmail.com>
2023-09-29 21:17:15 +0300

    first commit

---
+++ src/lesson2/Calculator.java
@@ -0,0 +1,37 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        calculate();
+    }
+
+    public static void calculate() {
+        System.out.println("Enter the num: ");
+
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextInt();
+
+        System.out.println("Enter the second num: ");
+        double b = scanner.nextInt();
+
+        System.out.println("Choose the operator + - * /: ");
+        String c = scanner.next();
+
+        System.out.println("The result of addition these numbers");
+        switch (c) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+
+        }
+
+        scanner.close();
+    }
+
+}
