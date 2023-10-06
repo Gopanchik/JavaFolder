e892b912c858e5f850b8162c4b8eaf96e18c7132
sanyo <egp.2277@gmail.com>
2023-10-06 16:34:30 +0300

    Third Home Work

--- src/lesson2/Calculator.java
+++ src/lesson2/Calculator.java
@@ -9,34 +9,57 @@ public class Calculator {
     }
 
     public static void calculate() {
-        System.out.println("Enter the num: ");
-
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextInt();
 
-        System.out.println("Enter the second num: ");
-        double b = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter the first num: ");
+            double a = scanner.nextDouble();
 
-        System.out.println("Choose the operator + - * /: ");
-        String c = scanner.next();
+            System.out.print("Enter the second num: ");
+            double b = scanner.nextDouble();
 
-        System.out.println("The result of addition these numbers");
-        switch (c) {
-            case "+":
-                System.out.println(a + b);
+            System.out.print("Choose the operator (+ - * /:) [or enter 'q' for exit]   ");
+            String operator = scanner.next();
+
+            if (operator.equals("q")) {
+                System.out.println("The app is closed.");
                 break;
-            case "-":
-                System.out.println(a - b);
-                break;
-            case "*":
-                System.out.println(a * b);
-                break;
-            case "/":
-                System.out.println(a / b);
-                break;
+            }
+
+            double result = 0;
+
+            switch (operator) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    if (b != 0) {
+                        result = a / b;
+                    } else {
+                        System.out.println("Error! You can't divide by zero");
+                        System.out.println("Do you want to change numbers? \n ('y' for 'yes' / 'n' for 'no')");
+                        String yn = scanner.next();
+
+                        if (yn.equalsIgnoreCase("y")) {
+                            continue;
+                        } else if (yn.equalsIgnoreCase("n")) {
+                            System.exit(0);
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Error: wrong operator");
+            }
+
+            System.out.println("The result is: " + result);
         }
 
         scanner.close();
     }
-
 }
